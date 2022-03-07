7e8c1e1a2498b72f0b327fc6bf8aa3e004f7b5c6
Phat Pham <phatpham01228@gmail.com>
2022-03-07 19:43:05 +0700

    test terraform

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,14 +5,21 @@ resource "aws_s3_bucket" "csv_bucket" {
   force_destroy = true
 }
 
-
-resource "aws_s3_bucket" "athena_query_s3" {
+data "aws_s3_bucket" "athena_query_s3_bucket"{
   count = var.environment == "qa" ? 0 : 1
-  bucket = "aws-athena-query-results-s3"
+  bucket= "aws-athena-query-results-s3"
 }
 
+
+
+resource "aws_s3_bucket" "athena_query_s3" {
+  bucket = data.aws_s3_bucket.athena_query_s3_bucket[0].bucket
+}
+
+
+
 resource "aws_s3_bucket_public_access_block" "athena_query_s3" {
-  bucket                  = aws_s3_bucket.athena_query_s3[*].id
+  bucket                  = data.aws_s3_bucket.athena_query_s3_bucket[0].id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
