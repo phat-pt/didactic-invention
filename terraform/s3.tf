3c515182c2cfd3933c9435182e60af5bbf213358
Phat Pham <phatpham01228@gmail.com>
2022-03-07 17:59:40 +0700

    testing terraform

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,3 +4,17 @@ resource "aws_s3_bucket" "csv_bucket" {
   bucket = "csv-bucket"
   force_destroy = true
 }
+
+
+resource "aws_s3_bucket" "query_s3" {
+  count  = var.environment == "qa" ? 0 : 1
+  bucket = "abc-s3"
+}
+
+resource "aws_s3_bucket_public_access_block" "query_s3_policy" {
+  bucket = aws_s3_bucket.query_s3[0].id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
