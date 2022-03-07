4d955004a9d5ece0ad33b09024a1e59529ae2f22
Phat Pham <phatpham01228@gmail.com>
2022-03-07 18:18:29 +0700

    test count terraform

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -7,16 +7,16 @@ resource "aws_s3_bucket" "csv_bucket" {
 
 
 data "aws_s3_bucket" "athena_query_s3_bucket"{
-  count = var.environment == "qa" ? 0 : 1
   bucket= "aws-athena-query-results-s3"
 }
 
 resource "aws_s3_bucket" "athena_query_s3" {
-  bucket = data.aws_s3_bucket.athena_query_s3_bucket[0].bucket
+  count = var.environment == "qa" ? 0 : 1
+  bucket = data.aws_s3_bucket.athena_query_s3_bucket.bucket
 }
 
 resource "aws_s3_bucket_public_access_block" "athena_query_s3" {
-  bucket                  = data.aws_s3_bucket.athena_query_s3[0].id
+  bucket                  = aws_s3_bucket.athena_query_s3[0].id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
