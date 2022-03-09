62e255f2209a4442ad804ce379c6db8a2222a641
Phat Pham <phatpham01228@gmail.com>
2022-03-09 10:55:58 +0700

    remove error code

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,21 +4,3 @@ resource "aws_s3_bucket" "csv_bucket" {
   bucket = "csv-bucket"
   force_destroy = true
 }
-
-data "aws_s3_bucket" "athena_query_s3_bucket"{
-  count = var.environment == "qa" ? 0 : 1
-  bucket= "csv_bucket"
-}
-
-resource "aws_s3_bucket" "athena_query_s3" {
-  bucket = data.aws_s3_bucket.athena_query_s3_bucket.bucket
-}
-
-
-resource "aws_s3_bucket_public_access_block" "athena_query_s3" {
-  bucket                  = data.aws_s3_bucket.athena_query_s3_bucket.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
